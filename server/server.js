0523456f5d4ef9d9083088bf9dd32ba0a7f08dd8
Liftraffic <lifttrafficagency@gmail.com>
2025-07-24 12:12:42 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -34,11 +34,17 @@ app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 
 // mysql
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "leadcrm",
+// });
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "leadcrm",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // middleware
