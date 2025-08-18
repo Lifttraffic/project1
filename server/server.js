5d58ad4fb97bd7df96613ddaa568fc6cd302c8d7
Lifttraffic <lifttrafficagency@gmail.com>
2025-08-18 21:06:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
-import express from "express";
-import mysql from "mysql";
 import cors from "cors";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
