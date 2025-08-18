c9407736508b80510e161fce6b79a99353e8f60f
Lifttraffic <lifttrafficagency@gmail.com>
2025-08-18 21:04:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const mysql = require('mysql');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mysql from "mysql";
