28c0145fded01016c92b8939737971f7b91224af
Lifttraffic <lifttrafficagency@gmail.com>
2025-08-24 23:59:17 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,13 +1,15 @@
-const mysql = require("mysql2");
+// server/db.js
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "leadcrm",
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-module.exports = db;
+module.exports = pool;
